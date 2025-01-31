b9ecb8b42aaa1fe13b743e6c9e67e99c202f3c77
sanathk7 <sakum@eg.dk>
2025-01-31 14:44:33 +0530

    first commit

---
+++ Pages/webView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace AppiumSpecFlowProject1.Pages
+{
+    public class webView
+    {
+        public webView(AndroidDriver androidDriver)
+        {
+
+            PageFactory.InitElements(androidDriver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//android.widget.TextView[@text=\"󰖟\"]")]
+        private IWebElement webViewButton { get; set; }
+        public void webClick()
+        {
+            webViewButton.Click();
+        }
+    }
+}
